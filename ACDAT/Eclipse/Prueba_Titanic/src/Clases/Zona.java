5e019ab247cb6196f7e68388b9da7b95646403d8
David <daviderninja@gmail.com>
2023-09-20 14:02:06 +0200

    Continuacion Titanic java 2

--- ACDAT/Eclipse/Prueba_Titanic/src/Clases/Zona.java
+++ ACDAT/Eclipse/Prueba_Titanic/src/Clases/Zona.java
@@ -21,47 +21,34 @@ public enum Zona {
 	}
 	*/
 	
-	Proa(), Popa(), Babor() ,Estribor();
+	Proa(1,2,3), Popa(4,5,6), Babor(7,8,9) ,Estribor(10,11,12);
 	
-	private int[] escalera ;
+	private int escalera ;
 	
 
-	Zona(){
+	Zona(int num1, int num2, int num3 ){
 		Random random = new Random(); 
 		
-		ArrayList<Integer> lista1 = new ArrayList<Integer>();
-		lista1.add(1);
-		lista1.add(2);
-		lista1.add(3);
+		ArrayList<Integer> lista = new ArrayList<Integer>();
+		lista.add(num1);
+		lista.add(num2);
+		lista.add(num3);
 
-		ArrayList<Integer> lista2 = new ArrayList<Integer>();
-		lista2.add(4);
-		lista2.add(5);
-		lista2.add(6);
 		
-		ArrayList<Integer> lista3 = new ArrayList<Integer>();
-		lista3.add(7);
-		lista3.add(8);
-		lista3.add(9);
-		
-		ArrayList<Integer> lista4 = new ArrayList<Integer>();
-		lista4.add(10);
-		lista4.add(11);
-		lista4.add(12);
 		
         // Generar un número aleatorio entre los tres números
         int numeroAleatorio = random.nextInt(3); // Genera un número entre 0 (inclusive) y 3 (exclusivo)
 
         // Sumar el número aleatorio al primer número para obtener un número entre los tres
-        int resultado =lista1.get(numeroAleatorio);
+        int resultado =lista.get(numeroAleatorio);
 
         
 		
-		this.escalera = escalera;
+		this.escalera = resultado;
 	}
 	
 	
-	public int[] getescalera(){
+	public int getescalera(){
 		return escalera;
 	}
 	
