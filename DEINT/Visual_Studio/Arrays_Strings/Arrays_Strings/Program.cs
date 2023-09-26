b77aed3fe2021ed4c273d6d76d93660c857232f7
David <daviderninja@gmail.com>
2023-09-26 13:24:44 +0200

    Ejercicios DEINT T2 EJ4

---
+++ DEINT/Visual_Studio/Arrays_Strings/Arrays_Strings/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Arrays_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arrays");
+
+            //indicamos el tipo
+
+            int[] array = new int[10];
+
+            int[] array2 = new int[] { 1,2,3,4,5,6,7,8};
+
+            int[,] matriz = new int[3,4]; //tamaño del primero y del segundo
+
+            matriz[0, 0] = 0;
+
+
+            for (int i = 0; i < matriz.Length; i++)
+            {
+
+                for (int j = 0; j < matriz.Length; j++)
+                {
+                    Console.Write(array[i]);
+                }    
+
+            }
+
+
+            array[^1] = 0; //Esto es la ultima
+
+
+            array[..3];
+
+        }
+    }
+}
---
+++ DEINT/Visual_Studio/T2_E4_Arrays/Actividad4/Actividad4/Arrays.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad4
+{
+    public class Arrays
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int minValorArray(int[] array)
+        {
+            //TODO
+            if (array.Length == 0)
+            {
+                
+                int minV = 0;
+                return minV;
+            }
+            else {
+
+                int minV = array[0]; ;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+
+
+                    if (minV > array[i])
+                    {
+
+                        minV = array[i];
+
+                    }
+
+                }
+                return minV;
+            }
+            
+
+            
+
+            throw new NotImplementedException();
+        }
+        public static int[] invertirArray(int[] array)
+        {
+            //TODO
+
+            int temp =  0,temp2 = 0;
+
+            for(int i = 0; i < array.Length; i++) { 
+            
+                temp = array[i];
+                temp2 = array[^(i+1)];
+
+                array[i] = temp2;
+                array[^(i+1)] = temp;
+
+            }
+
+            return array;
+
+                throw new NotImplementedException();
+        }
+
+        public static int comprobarValorEnArray(int[] array, int valor)
+        {
+            //TODO
+
+            int salida = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                if (array[i] == valor)
+                {
+
+                    return i;
+                }
+                
+            }
+
+            return salida;
+
+
+            throw new NotImplementedException();
+        }
+
+        public static int calcularModa(int[] entrada)
+        {
+
+            //Mapa en C#
+            Dictionary<int, int> frecuencias = new Dictionary<int, int>();
+
+            // Contar la frecuencia de cada elemento en el array
+            foreach (int numero in entrada)
+            {
+                if (frecuencias.ContainsKey(numero))
+                {
+                    frecuencias[numero]++;
+                }
+                else
+                {
+                    frecuencias[numero] = 1;
+                }
+            }
+
+            int moda = 0;
+            int maxFrecuencia = 0;
+
+            // Encontrar el elemento con la frecuencia máxima
+            foreach (var kvp in frecuencias)
+            {
+                if (kvp.Value > maxFrecuencia)
+                {
+                    moda = kvp.Key;
+                    maxFrecuencia = kvp.Value;
+                }
+            }
+
+            return moda;
+
+
+
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
