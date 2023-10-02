6d09285135c2ed04fb79b3308e87b38c9c8d244d
David <daviderninja@gmail.com>
2023-10-02 13:23:05 +0200

    DEINT

--- DEINT/Visual_Studio/PilasyColas/Ejercicio1_PilasYColas/Program.cs
+++ DEINT/Visual_Studio/PilasyColas/Ejercicio1_PilasYColas/Program.cs
@@ -32,32 +32,37 @@
             for (int i = 0; i < caracteres.Length; i++) 
             {
 
-                if (caracteres[i].Equals("(") || caracteres[i].Equals("[") || caracteres[i].Equals("{"))
+                Console.WriteLine(caracteres[i]);
+
+                if (caracteres[i].Equals('(') || caracteres[i].Equals('[') || caracteres[i].Equals('{'))
                 {
 
                     pila.Push(caracteres[i]);
 
 
 
-                } else if (caracteres[i].Equals(")") || caracteres[i].Equals("]") || caracteres[i].Equals("}")) 
+                } 
+                else if (caracteres[i].Equals(')') || caracteres[i].Equals(']') || caracteres[i].Equals('}')) 
                 {
 
-                    if (caracteres[i].Equals(")"))
+                    if (caracteres[i].Equals(')'))
                     {
-                        if (pila.Peek().Equals("(")) { pila.Pop(); } 
-                    } 
-                    else if (caracteres[i].Equals("]")) 
-                    {
-                        if (pila.Peek().Equals("[")) { pila.Pop(); }
+                        if (pila.Peek().Equals('(')) { pila.Pop(); }
                     }
-                    else if (caracteres[i].Equals("}"))
+                    
+                    if (caracteres[i].Equals(']'))
                     {
-                        if (pila.Peek().Equals("{")) { pila.Pop(); }
+                        if (pila.Peek().Equals('[')) { pila.Pop(); }
                     }
-                    else
+                    
+                    if (caracteres[i].Equals('}'))
                     {
-                        correcto = false;
+                        if (pila.Peek().Equals('{')) { pila.Pop(); }
                     }
+
+                    
+                    else { correcto = false; break; }
+                    
                     
 
                 }
@@ -71,3 +76,94 @@
         }
     }
 }
+
+/*
+ LEER
+
+Para realizar este ejercicio, puedes seguir los siguientes pasos:
+
+Ingreso de la fórmula: Primero, solicita al usuario que ingrese una fórmula que contenga paréntesis, 
+corchetes y llaves. Por ejemplo, el usuario podría ingresar "(2+[3-12]*{8/3})".
+
+Validación de los paréntesis, corchetes y llaves: A continuación, debes verificar si estos caracteres 
+están balanceados correctamente. Para hacerlo, puedes utilizar una pila (stack) para realizar un seguimiento 
+de los caracteres de apertura mientras recorres la fórmula.
+
+Recorrido de la fórmula: Recorre cada carácter de la fórmula uno por uno.
+
+Si es un carácter de apertura: Si el carácter es uno de los caracteres de apertura (paréntesis, corchete o llave), agrégalo a la pila.
+
+Si es un carácter de cierre: Si el carácter es uno de los caracteres de cierre, verifica si la pila está vacía.
+Si está vacía, significa que no hay un carácter de apertura correspondiente y la fórmula no está balanceada correctamente. 
+Si la pila no está vacía, saca el último carácter de la pila y verifica si coincide con el carácter de cierre actual. 
+Si no coinciden, la fórmula no está balanceada correctamente.
+
+Al final del recorrido: Después de recorrer toda la fórmula, verifica si la pila está vacía. Si está vacía, significa 
+que todos los caracteres de apertura tienen sus correspondientes caracteres de cierre y la fórmula está balanceada correctamente.
+
+Aquí tienes un ejemplo de código en C# que implementa este proceso:
+
+csharp
+Copy code
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Por favor, ingresa una fórmula:");
+        string formula = Console.ReadLine();
+
+        if (EstaBalanceada(formula))
+        {
+            Console.WriteLine("La fórmula está balanceada correctamente.");
+        }
+        else
+        {
+            Console.WriteLine("La fórmula no está balanceada correctamente.");
+        }
+    }
+
+    static bool EstaBalanceada(string formula)
+    {
+        Stack<char> pila = new Stack<char>();
+
+        foreach (char caracter in formula)
+        {
+            if (EsCaracterDeApertura(caracter))
+            {
+                pila.Push(caracter);
+            }
+            else if (EsCaracterDeCierre(caracter))
+            {
+                if (pila.Count == 0 || !Corresponden(pila.Pop(), caracter))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return pila.Count == 0;
+    }
+
+    static bool EsCaracterDeApertura(char c)
+    {
+        return c == '(' || c == '[' || c == '{';
+    }
+
+    static bool EsCaracterDeCierre(char c)
+    {
+        return c == ')' || c == ']' || c == '}';
+    }
+
+    static bool Corresponden(char apertura, char cierre)
+    {
+        return (apertura == '(' && cierre == ')') ||
+               (apertura == '[' && cierre == ']') ||
+               (apertura == '{' && cierre == '}');
+    }
+}
+Este código solicitará al usuario que ingrese una fórmula y luego verificará si los paréntesis, corchetes y llaves están balanceados correctamente. Si lo están, mostrará un mensaje de que la fórmula es válida; de lo contrario, mostrará un mensaje de que no es válida.
+
+ */
