4d56c158fb0457ebc87780801358bf8c3c646848
David <daviderninja@gmail.com>
2023-10-20 13:25:17 +0200

    Ejercicio de pablo sobre imput, poryectar codigo y incrustarlo

--- PMDMO/Angular/Carrusel/Carrusel/src/app/cabecera/cabecera.component.ts
+++ PMDMO/Angular/Carrusel/Carrusel/src/app/cabecera/cabecera.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-cabecera',
+  selector: 'app-cabecera-imput',
   templateUrl: './cabecera.component.html',
   styleUrls: ['./cabecera.component.css']
 })
 export class CabeceraComponent {
+
+  @Input() logoUrl!:string;
 
 }
--- PMDMO/Angular/Carrusel/Carrusel/src/app/cabecera3/cabecera3.component.ts
+++ PMDMO/Angular/Carrusel/Carrusel/src/app/cabecera3/cabecera3.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-cabecera3',
+  selector: 'app-cabecera3-codigo',
   templateUrl: './cabecera3.component.html',
   styleUrls: ['./cabecera3.component.css']
 })
 export class Cabecera3Component {
+
+  
+
+  //@ViewChild('img') cabecera: ElementRef;
+  
 
 }
