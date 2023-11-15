ceed4eeb6b8a327da15056218692c2b6a5dda78d
DaMSet <daviderninja@gmail.com>
2023-11-15 01:58:24 +0100

    -

--- DEINT/Visual_Studio/U3_E5_Ficheros/U3_E5_5_Ficheros/Form1.Designer.cs
+++ DEINT/Visual_Studio/U3_E5_Ficheros/U3_E5_5_Ficheros/Form1.Designer.cs
@@ -28,19 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            T = new FileSystemWatcher();
+            txtArchivo1 = new TextBox();
+            txtArchivo2 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)T).BeginInit();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(341, 292);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // T
+            // 
+            T.EnableRaisingEvents = true;
+            T.SynchronizingObject = this;
+            // 
+            // txtArchivo1
+            // 
+            txtArchivo1.Location = new Point(132, 171);
+            txtArchivo1.Name = "txtArchivo1";
+            txtArchivo1.Size = new Size(100, 23);
+            txtArchivo1.TabIndex = 1;
+            // 
+            // txtArchivo2
+            // 
+            txtArchivo2.Location = new Point(563, 171);
+            txtArchivo2.Name = "txtArchivo2";
+            txtArchivo2.Size = new Size(100, 23);
+            txtArchivo2.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(txtArchivo2);
+            Controls.Add(txtArchivo1);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)T).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private FileSystemWatcher T;
+        private TextBox txtArchivo2;
+        private TextBox txtArchivo1;
     }
 }
--- DEINT/Visual_Studio/U3_E5_Ficheros/U3_E5_5_Ficheros/Form1.cs
+++ DEINT/Visual_Studio/U3_E5_Ficheros/U3_E5_5_Ficheros/Form1.cs
@@ -11,6 +11,50 @@ namespace U3_E5_5_Ficheros
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string inputFile1 = Path.Combine(Path.GetDirectoryName(Environment.CurrentDirectory), $"{txtArchivo1.Text.ToString()}.txt");
+            string inputFile2 = Path.Combine(Path.GetDirectoryName(Environment.CurrentDirectory), $"{txtArchivo2.Text.ToString()}.txt");
+            string outputFile = Path.Combine(Path.GetDirectoryName(Environment.CurrentDirectory), "union.txt");
+
+            try
+            {
+                CombineFiles(inputFile1, inputFile2, outputFile);
+                Console.WriteLine("La combinación de archivos se ha realizado con éxito. El resultado se encuentra en 'union.txt'");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        static void CombineFiles(string inputFile1, string inputFile2, string outputFile)
+        {
+            using (StreamReader reader1 = new StreamReader(inputFile1))
+            using (StreamReader reader2 = new StreamReader(inputFile2))
+            using (StreamWriter writer = new StreamWriter(outputFile,true))
+            {
+                while (!reader1.EndOfStream || !reader2.EndOfStream)
+                {
+                    if (!reader1.EndOfStream)
+                    {
+                        string line1 = reader1.ReadLine();
+                        writer.WriteLine(line1);
+                    }
+
+                    if (!reader2.EndOfStream)
+                    {
+                        string line2 = reader2.ReadLine();
+                        writer.WriteLine(line2);
+                    }
+                }
+            }
+
+        }
+
+
     }
 }
 /*
