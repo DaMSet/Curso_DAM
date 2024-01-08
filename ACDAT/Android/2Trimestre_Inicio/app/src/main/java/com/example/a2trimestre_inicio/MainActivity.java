5ff42794f9928375a5eac0c146a9af4cfb1f8b34
David <daviderninja@gmail.com>
2024-01-08 09:43:35 +0100

    ---

--- ACDAT/Android/2Trimestre_Inicio/app/src/main/java/com/example/a2trimestre_inicio/MainActivity.java
+++ ACDAT/Android/2Trimestre_Inicio/app/src/main/java/com/example/a2trimestre_inicio/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         protected void onDraw(Canvas canvas)
         {
             canvas.drawRGB(rdn.nextInt(256),rdn.nextInt(256),rdn.nextInt(256));
+
         }
 
     }
@@ -34,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
