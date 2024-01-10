a4dcc16f6f0a14d08306c8a2f98c3188a74a862e
David <daviderninja@gmail.com>
2024-01-10 11:52:54 +0100

    --

---
+++ DEINT/Visual_Studio/MauiAppEjemplo/MauiAppEjemplo/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MauiAppEjemplo.Models;
+
+namespace MauiAppEjemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            var person = new Person
+            {
+                Name = "nombre",
+                Adress = "sitio",
+                Phone = 1,
+
+            };
+
+            BindingContext =  person;
+
+        }
+
+
+
+
+    }
+}
---
+++ DEINT/Visual_Studio/MauiAppEjemplo/MauiAppEjemplo/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppEjemplo.Models
+{
+
+    //DATABINDING DEMO
+    //label.text --> source -- p.name
+    internal class Person
+    {
+
+        public string Name { get; set; }
+        public int Phone { get; set; }
+        public string Adress { get; set; }
+
+
+
+    }
+}
